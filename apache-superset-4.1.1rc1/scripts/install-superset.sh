faa136312657f2d87fc94d0f844dbfff2dd74944
devops-opsonspot <devops@opsonspot.com>
2024-12-16 01:21:00 -0600

    change script and dockerfile and add image

--- apache-superset-4.1.1rc1/scripts/install-superset.sh
+++ apache-superset-4.1.1rc1/scripts/install-superset.sh
@@ -4,10 +4,10 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Move to the superset-frontend directory and execute the necessary commands
-cd "$SCRIPT_DIR/../superset3.0/superset-frontend/packages/generator-superset/custom_plugins/superset-custom-table-downtime-breakdown" && npm i && npm run build
-cd "$SCRIPT_DIR/../superset3.0/superset-frontend/packages/generator-superset/custom_plugins/superset-plugin-table-extension" && npm i && npm run build
-cd "$SCRIPT_DIR/../superset3.0/superset-frontend/packages/generator-superset/custom_plugins/superset-plugin-table-popup-button" && npm i && npm run build
- cd "$SCRIPT_DIR/../superset3.0/superset-frontend/" && npm i --f
+cd "$SCRIPT_DIR/../superset-frontend/packages/generator-superset/custom_plugins/superset-custom-table-downtime-breakdown" && npm i && npm run build
+cd "$SCRIPT_DIR/../superset-frontend/packages/generator-superset/custom_plugins/superset-plugin-table-extension" && npm i && npm run build
+cd "$SCRIPT_DIR/../superset-frontend/packages/generator-superset/custom_plugins/superset-plugin-table-popup-button" && npm i && npm run build
+ cd "$SCRIPT_DIR/../superset-frontend/" && npm i --f
 
 # Move back to the directory containing the script and execute docker-compose command
 cd "$SCRIPT_DIR"
