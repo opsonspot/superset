9d8d8526a95a1d64b95a8b21351f466e91758713
devops-opsonspot <devops@opsonspot.com>
2024-12-18 01:32:32 -0600

    comment url

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -96,9 +96,9 @@ APP_ICON = "/static/assets/superset-logo-horiz.png"
 FAVICONS=[{"href": "/static/assets/favicon.png"}]
 FEATURE_FLAGS = {"ALERT_REPORTS": True}
 ALERT_REPORTS_NOTIFICATION_DRY_RUN = True
-WEBDRIVER_BASEURL = "http://superset:8088/"
+# WEBDRIVER_BASEURL = "http://superset:8088/"
 # The base URL for the email report hyperlinks.
-WEBDRIVER_BASEURL_USER_FRIENDLY = WEBDRIVER_BASEURL
+# WEBDRIVER_BASEURL_USER_FRIENDLY = WEBDRIVER_BASEURL
 
 SQLLAB_CTAS_NO_LIMIT = True
 
