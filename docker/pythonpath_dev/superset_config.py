2802751d9ac603547f2ca34401f096224a2c876f
devops-opsonspot <devops@opsonspot.com>
2024-12-20 14:50:53 -0600

    script anme

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -97,7 +97,7 @@ FAVICONS=[{"href": "https://woodfrog.tech/favicon.ico"}]
 FEATURE_FLAGS = {"ALERT_REPORTS": True}
 ALERT_REPORTS_NOTIFICATION_DRY_RUN = True
 SCRIPT_NAME = '/woodfrog'
-WEBDRIVER_BASEURL = "http://superset:8080/"
+WEBDRIVER_BASEURL = "http://superset:8080/woodfrog"
 ENABLE_PROXY_FIX = True
 # The base URL for the email report hyperlinks.
 WEBDRIVER_BASEURL_USER_FRIENDLY = WEBDRIVER_BASEURL
